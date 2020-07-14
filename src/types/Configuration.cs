5b296487eebbb9a688cc83307051a81620f14c53
Kim Nerli <kim-andre-skole@hotmail.com>
2020-07-14 10:19:39 +0200

    Added default value to summary

--- src/types/Configuration.cs
+++ src/types/Configuration.cs
@@ -3,13 +3,16 @@ namespace Sharesol
   /// <summary>Type class to contain the application config.</summary>
   public class Configuration
   {
-    /// <summary>An integer port for the remote server, this is used by the client.</summary>
+    /// <summary>An integer port for the remote server, this is used by the client. Default: 11000</summary>
     public int RemotePort { get; set; } = 11000;
-    /// <summary>A Remote IP as a string, this is used by the client.</summary>
+
+    /// <summary>A Remote IP as a string, this is used by the client. Default: 127.0.0.1</summary>
     public string RemoteIP { get; set; } = "127.0.0.1";
-    /// <summary>An integer port for the local server, this is used by the remote client.</summary>
+
+    /// <summary>An integer port for the local server, this is used by the remote client. Default: 11000</summary>
     public int LocalPort { get; set; } = 11000;
-    /// <summary>The local server IP as a string.</summary>
+
+    /// <summary>The local server IP as a string. Default: 127.0.0.1</summary>
     public string LocalIP { get; set; } = "127.0.0.1";
   }
 }
